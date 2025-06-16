079c25cdf61ca70109be9fa91eeefa2cde3910d8
gauri123123 <bagaweg2000@gmail.com>
2025-06-16 22:09:49 +0530

    changes for live dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LiveDashboardComponent } from './liveDashboard/live-dashboard/live-dashboard';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{ path: '', component: Home },
+  { path: 'live-dashboard', component: LiveDashboardComponent }
+];
