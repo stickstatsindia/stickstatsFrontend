ce7655f387f87b62aced354a13a8fd67486b0790
2123-suneha <sunehasudan@gmail.com>
2025-06-23 23:52:04 +0530

    add tournament updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { About } from './about/about';
 import { Contact } from './contact/contact';
 import { ProfileForm } from './profile-form/profile-form';
 import { LiveDashboardComponent } from './liveDashboard/live-dashboard/live-dashboard';
+import { AddTournamentComponent } from './add-tournament.component/add-tournament.component';
 
 
 export const routes: Routes = [
@@ -14,5 +15,7 @@ export const routes: Routes = [
   { path: 'about', component: About },
   { path: 'contact', component: Contact },
   { path: 'live-dashboard', component: LiveDashboardComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
+  { path: 'add tournament', component: AddTournamentComponent }
+  
 ];
