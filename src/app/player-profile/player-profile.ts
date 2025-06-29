ded7cc066c3665cb7d235921e87b858f46cc1403
Manpreet-kaur-1313 <manpreetkaur111200@gmail.com>
2025-06-29 23:08:20 +0530

    created player profile component

---
+++ src/app/player-profile/player-profile.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-profile',
+  imports: [],
+  templateUrl: './player-profile.html',
+  styleUrl: './player-profile.css'
+})
+export class PlayerProfile {
+
+}
