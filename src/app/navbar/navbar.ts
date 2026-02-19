831186adfc8986ee3526ba3f659d7d12f1703d7d
evejeet123 <evejeetsodhi123@gmail.com>
2026-02-19 23:11:09 +0530

    navbar auto close issue resolve

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription, filter } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -9,14 +11,40 @@ import { CommonModule } from '@angular/common';
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
-export class Navbar {
+export class Navbar implements OnInit, OnDestroy {
   isLoggedIn: boolean = false;
+  isMenuOpen: boolean = false;
+  private routerSub?: Subscription;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.routerSub = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.isMenuOpen = false;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.routerSub?.unsubscribe();
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
 
   onLogin(): void {
     this.isLoggedIn = true;
+    this.closeMenu();
   }
 
   onLogout(): void {
     this.isLoggedIn = false;
+    this.closeMenu();
   }
 }
