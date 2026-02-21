b1b2db616fbc86df53007ad4748d05152af6bdea
evejeet123 <evejeetsodhi123@gmail.com>
2026-02-21 18:08:19 +0530

    about

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [RouterModule],
   templateUrl: './about.html',
   styleUrls: ['./about.css']
 })
-export class About {}
+export class About {
+}
